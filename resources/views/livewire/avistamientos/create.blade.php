36fab9a7c32b2d0d78c1b8e437d3470ebd8af8ba
Emilio Vargas Millan <emiliovm@outlook.es>
2024-11-03 23:22:32 +0100

    style: quitado uppercase, hecho de back

--- resources/views/livewire/avistamientos/create.blade.php
+++ resources/views/livewire/avistamientos/create.blade.php
@@ -24,7 +24,7 @@
             <div class="mb-5">
                 <label for="matricula"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Matricula</label>
-                <input style="text-transform: uppercase;" wire:model.live.debounce.250ms='matricula' type="text"
+                <input wire:model.live.debounce.250ms='matricula' type="text"
                     id="matricula"
                     class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                     placeholder="1234MNC" />
