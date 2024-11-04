5e0c080b6903339635fa4ca1f5270acf5555112e
Emilio Vargas Millan <emiliovm@outlook.es>
2024-11-04 21:32:52 +0100

    feat: cambiado texto busqueda matricula

--- resources/views/matriculas/status/index.blade.php
+++ resources/views/matriculas/status/index.blade.php
@@ -2,9 +2,8 @@
     <div>
         <div class="flex justify-center items-center flex-col p-10">
             <h1 class="dark:text-gray-300 text-xl text-balance my-10 md:text-center">
-                En este apartado, debes introducir la mátricula que deseas buscar.
-                Tras ello, podrás utilizar tu correo electrónico para poder realizar consultas posteriores al estado de tu
-                búsqueda.
+                En este apartado, introduce el correo electrónico que especificaste a la hora de
+                introducir la matrícula para poder ver si tiene alguna matricula en nuestros registros.
             </h1>
             <livewire:matriculas.consultar />
         </div>
